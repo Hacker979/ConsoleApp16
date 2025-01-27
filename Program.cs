05ef9d6637cf1d35dc7b1c42bb437c1029c3c5d6
jotaro4u <kothaker135@gmail.com>
2025-01-28 00:33:48 +0300

    vb

--- Program.cs
+++ Program.cs
@@ -10,68 +10,21 @@ namespace ConsoleApp16
     {
         static void var_a()
         {
-            Console.WriteLine("вариант a");
             for (int x = 0; x <= 3; x++)
             {
-                for (int i = 1; i <= 10; i++)
+                for (int i = 1; i <= 7; i++)
                 {
-                    Console.Write($"{i} ");
+                    Console.Write($"#");
                 }
-                Console.WriteLine();
+                Console.WriteLine("\n");
             }
             
-        }
-        static void var_b()
-        {
-            Console.WriteLine("вариант б");
-            int i = 40;
-            for (int x = 0 ; x <= 3; x++)
-            {
-                for  (int y = 0 ; y <= 9; y++)
-                {
-                    i++;
-                    Console.Write($"{i} ");
-                }
-                Console.WriteLine();
-            }
-
-        }
-        static void var_v()
-        {
-            Console.WriteLine("вариант в");
-            for (int x = 0; x <= 4; x++)
-            {
-                for (int y = 0; y <= x; y++)
-                {
-                    Console.Write($"5 ");
-                }
-                Console.WriteLine();
-            }
-
-        }
-        static void var_g()
-        {
-            Console.WriteLine("вариант г");
-            for (int x = 4; x >= 0; x--)
-            {
-                for (int y = 0; y <= x; y++)
-                {
-                    Console.Write($"1 ");
-                }
-                Console.WriteLine();
-            }
-
         }
 
         static void Main(string[] args)
         {
 
             var_a();
-            var_b();
-            var_v();
-            var_g();
-
-
             Console.Read();
 
         }
